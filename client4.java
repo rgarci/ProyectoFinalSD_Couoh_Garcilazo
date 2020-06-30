7777fd245b636a14566f99178e60724ba8ed3fc8
rosi <rosigarcilazo@gmail.com>
2020-06-29 19:43:35 -0500

    actualizar video

--- client4.java
+++ client4.java
@@ -2,22 +2,21 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.time.LocalDateTime;
 
-public class client2 {
+public class client4 {
     public static void main(String[] args) {
         try {
             Registry registry = LocateRegistry.getRegistry("127.0.0.1");
             IRemoteLogin login = (IRemoteLogin) registry.lookup("Login");
             IRemoteTransaction rt = login.login();
 
-            Transaction trU2 = new Transaction();
-            trU2.setRFCCom("BBBBBB2222");
-            trU2.setRFCUsr("USER222222");
-            trU2.setOperationCode(-1);
-            trU2.setNoAc(50);
-            trU2.setOperationDate(LocalDateTime.now());
-            trU2.setOperationPrice(500.00);
-            System.out.println(rt.ofertar(trU2)?"ganador": "perdedor");
-            
+            Transaction trU3 = new Transaction();
+            trU3.setRFCCom("BBBBBB2222");
+            trU3.setRFCUsr("USER444444");
+            trU3.setOperationCode(1);
+            trU3.setNoAc(100);
+            trU3.setOperationDate(LocalDateTime.now());
+            trU3.setOperationPrice(550.00);
+            System.out.println(rt.ofertar(trU3)?"ganador":"perdedor");
         } catch (Exception e) {
             e.printStackTrace();
         }
